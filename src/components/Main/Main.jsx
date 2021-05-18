09e033d9e5a3c886f810629e30a316e5c6e8d30e
Will Trinh <williamltrinh@gmail.com>
2021-05-18 14:08:53 -0700

    Implemented income & expense components

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  Grid,
+  Divider,
+} from "@material-ui/core";
+import useStyles from "./styles.js";
+const Main = () => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <CardHeader title="Expense Tracker" />
+      <CardContent>
+        <Typography align="center" variant="h5">
+          Total Balance $100
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          style={{ lineHeight: "1.5rem", marginTop: "20px" }}
+        >
+          {/* InfoCard... */}
+          Try saying: Add Income for $100 in Category Salary for Monday...
+        </Typography>
+        <Divider />
+        {/* Form component */}
+      </CardContent>
+      <CardContent className={classes.cardContent}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            {/* List */}
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Main;
