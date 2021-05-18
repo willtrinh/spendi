deb2d835774385026579e305c48ba07c9abebb94
Will Trinh <williamltrinh@gmail.com>
2021-05-18 16:35:25 -0700

    Implemented add and delete actions for contextReducer

--- src/context/contextReducer.js
+++ src/context/contextReducer.js
@@ -1,6 +1,20 @@
 // Reducer - a function that takes in the old state and an action and return a new state.
 const contextReducer = (state, action) => {
+  let transactions;
+  switch (action.type) {
+    case 'ADD_TRANSACTION':
+      // add the new transaction at the top and spread older transactions below it
+      transactions = [action.payload, ...state];
+      return transactions;
+    case 'DELETE_TRANSACTION':
+      // filter out the id 
+      transactions = state.filter((transaction) => transaction.id !== action.payload);
+      return transactions;
+    default:
+      // simply return the state if no matched case
+      return state;
 
+  }
 }
 
 export default contextReducer;
