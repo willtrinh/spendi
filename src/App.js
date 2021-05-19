1c28d2038c55486d3b846ce46ce6f1d51919ab9d
Will Trinh <williamltrinh@gmail.com>
2021-05-19 13:45:50 -0700

    Added scrollToView for mobile

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import { SpeechState, useSpeechContext } from '@speechly/react-client';
 import { Grid } from '@material-ui/core';
 import { PushToTalkButton, PushToTalkButtonContainer, ErrorPanel } from '@speechly/react-ui';
 import Details from './components/Details/Details';
@@ -6,13 +7,22 @@ import Main from './components/Main/Main';
 import useStyles from './styles';
 const App = () => {
   const classes = useStyles();
+  const { speechState } = useSpeechContext();
+  const main = useRef(null);
+  const executeScroll = () => main.current.scrollIntoView();
+  // on mobile, while recording, scroll to view
+  useEffect(() => {
+    if (speechState === SpeechState.Recording) {
+      executeScroll();
+    }
+  }, [speechState])
   return (
     <>
       <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{ height: '100vh' }}>
         <Grid item xs={12} sm={3} className={classes.mobile}>
           <Details title="Income" />
         </Grid>
-        <Grid item xs={12} sm={3} className={classes.main}>
+        <Grid ref={main} item xs={12} sm={3} className={classes.main}>
           <Main />
         </Grid>
         <Grid item xs={12} sm={3} className={classes.desktop}>
