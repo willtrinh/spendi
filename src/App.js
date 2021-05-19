2ad38fab9d296f84e89b4ac8399aae0b259de535
Will Trinh <williamltrinh@gmail.com>
2021-05-19 15:44:02 -0700

    Revert "Added scrollToView for mobile"
    This reverts commit 1c28d2038c55486d3b846ce46ce6f1d51919ab9d.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useRef, useEffect } from 'react';
-import { SpeechState, useSpeechContext } from '@speechly/react-client';
+import React from 'react';
 import { Grid } from '@material-ui/core';
 import { PushToTalkButton, PushToTalkButtonContainer, ErrorPanel } from '@speechly/react-ui';
 import Details from './components/Details/Details';
@@ -7,22 +6,13 @@ import Main from './components/Main/Main';
 import useStyles from './styles';
 const App = () => {
   const classes = useStyles();
-  const { speechState } = useSpeechContext();
-  const main = useRef(null);
-  const executeScroll = () => main.current.scrollIntoView();
-  // on mobile, while recording, scroll to view
-  useEffect(() => {
-    if (speechState === SpeechState.Recording) {
-      executeScroll();
-    }
-  }, [speechState])
   return (
     <>
       <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{ height: '100vh' }}>
         <Grid item xs={12} sm={3} className={classes.mobile}>
           <Details title="Income" />
         </Grid>
-        <Grid ref={main} item xs={12} sm={3} className={classes.main}>
+        <Grid item xs={12} sm={3} className={classes.main}>
           <Main />
         </Grid>
         <Grid item xs={12} sm={3} className={classes.desktop}>
