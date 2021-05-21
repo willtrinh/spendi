56ee275da83301dd82c080075090d3b0a1eff900
Will Trinh <williamltrinh@gmail.com>
2021-05-21 12:38:56 -0700

    Made appId public to deploy the app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { SpeechProvider } from '@speechly/react-client';
-import { appId } from './config/config';
 import { Provider } from './context/context';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <SpeechProvider appId={appId} language="en-US">
+  <SpeechProvider appId="f6b383f7-52a4-4a80-97b8-72a258596951" language="en-US">
     <Provider>
       <App />
     </Provider>
